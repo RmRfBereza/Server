45c2a5088f982511ca52d87e7f26624289d212f3
LordNeznay <lordneznay@gmail.com>
2016-09-28 23:59:41 +0300

    [controller] fix YController

--- Assets/YControllers.cs
+++ Assets/YControllers.cs
@@ -1,74 +1,98 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class YControllers : MonoBehaviour {
-    public float dAcc = 0.1F;
+public class YControllers : MonoBehaviour 
+{
+    public float dAcc = 0.1F;           // Величина отклонения для активации
+    public int dS = 5;                  // Минимальное количество отклонений для активации
     
-    public float speed = 10.0F;
-    public float rotationSpeed = 100.0F;
+    public Vector3 top = Vector3.zero;  // Верхняя позиция
+    public Vector3 mid = Vector3.zero;  // Стандартная позиция  
+    public Vector3 bot = Vector3.zero;  // Нижняя позиция
     
-    private static int size = 250;
+    private const int size = 250;
     private float[] val = new float[size];
     private int lastVal = 0;
+    private int devation = 0;
     
-    void Start(){
-        for(int i=0; i < size; ++i){
+    void Start()
+    {
+        for(int i=0; i < size; ++i)
+        {
             val[i] = 0.0F;
         }
     }
     
-    float sr(){
+    float average()
+    {
         float result = 0.0F;
-        for(int i=0; i < size; ++i){
+        for(int i=0; i < size; ++i)
+        {
             result += val[i];
         }
         return result / size;
     }
     
-    void updLastVal(){
-        lastVal = lastVal + 1 == size ? 0 : lastVal + 1;
+    void updLastVal()
+    {
+        lastVal = (lastVal + 1) % size;
     }
     
-    bool bb(float a, float b){
+    bool isALargerB(float a, float b)
+    {
         return a - b > dAcc;
     }
-
-    int abs(int a){
-        return a > 0 ? a : -a;
+    
+    
+    void updateAverage()
+    {
+        val[lastVal] = Input.acceleration.y;
+        updLastVal();
     }
     
-    public Vector3 top = Vector3.zero;
-    public Vector3 mid = Vector3.zero;
-    public Vector3 bot = Vector3.zero;
-    
-    public int dS = 5;
-    private int step = 0;
-    
-    void Update() {
-        if (bb(sr(), Input.acceleration.y)){
-            --step;
-        } else if (bb(Input.acceleration.y, sr())){
-            ++step;
-        } else {
-            if(step < 0) ++step;
-            else --step;
+    void updateDevation()
+    {
+        if (isALargerB(average(), Input.acceleration.y))
+        {
+            --devation;
         }
-        
-        if (abs(step) < dS){
+        else if (isALargerB(Input.acceleration.y, average()))
+        {
+            ++devation;
+        }
+        else 
+        {
+            if(devation < 0) ++devation;
+            else --devation;
+        }
+    }
+    
+    void updatePosition()
+    {
+        if (Mathf.Abs(devation) < dS)
+        {
             Vector3 hz = transform.position;
             hz.y = mid.y;
             transform.position = hz;
-        } else if (step < 0){
+        }
+        else if (devation < 0)
+        {
             Vector3 hz = transform.position;
             hz.y = top.y;
             transform.position = hz;
-        } else {
+        }
+        else 
+        {
             Vector3 hz = transform.position;
             hz.y = bot.y;
             transform.position = hz;
         }
-        
-        val[lastVal] = Input.acceleration.y;
-        updLastVal();
+    }
+    
+    void Update()
+    {
+        updateDevation();
+        updatePosition();      
+        updateAverage();
     }
 }
