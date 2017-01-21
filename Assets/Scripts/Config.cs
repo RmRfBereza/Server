6578510b0dbeab7ba59d80d180e31560ac0bde10
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-01-21 14:33:15 +0300

    add LevelManager #4

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Config {
+    public const string SinglePlayerLevelsFileName = "SinglePlayerLevels.txt";
+    public const string MultiPlayerLevelsFileName = "MultiPlayerLevels.txt";
+}
