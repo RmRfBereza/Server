10df2150e0576798f51a6be9a030aaa3f2d2892a
LordNeznay <lordneznay@gmail.com>
2017-02-09 00:04:57 +0300

    [network] fix load scene from no-main thread

--- Assets/Scripts/Network/Client2.cs
+++ Assets/Scripts/Network/Client2.cs
@@ -11,19 +11,20 @@ using UnityEngine.SceneManagement;
 class Client2 : MonoBehaviour
 {
     BinaryFormatter formatter = new BinaryFormatter();
-	TcpClient client = null;
-	Stream s = null;
+    TcpClient client = null;
+    Stream s = null;
 
     public static string ip = "-1";
     public const string ipSkip = "s";
-	public static bool enableBase64 = true;
+    public static bool enableBase64 = true;
 
     Level level;
-	private volatile bool isNeedSend = true;
-	private bool inGame = false;
-	Thread t;
-			
-	void Start()
+    private volatile bool isNeedSend = true;
+    private volatile bool isNeedExit = false;
+    private bool inGame = false;
+    Thread t;
+            
+    void Start()
     {
         if (Config.isSingle)
         {
@@ -43,135 +44,137 @@ class Client2 : MonoBehaviour
         }
         if (ip == "-1") return;
 
-		t = new Thread(new ThreadStart(Client));
-		t.Start();
-	}
-	
-	void OnDestroy(){
-		t.Interrupt();
-	}
-	
-	void Client()
-	{
-		try{
-			if (enableBase64)
-			{
-				var base64EncodedBytes = System.Convert.FromBase64String(ip + "==");
-				var ipaddres = new IPAddress(base64EncodedBytes);
-				ip = ipaddres.ToString();
-				Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-				Debug.Log(ipaddres.ToString());
-				Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-			}
-			
-			client = new TcpClient(ip, 8865);
+        t = new Thread(new ThreadStart(Client));
+        t.Start();
+    }
+    
+    void OnDestroy(){
+        t.Interrupt();
+    }
+    
+    void Client()
+    {
+        try{
+            if (enableBase64)
+            {
+                var base64EncodedBytes = System.Convert.FromBase64String(ip + "==");
+                var ipaddres = new IPAddress(base64EncodedBytes);
+                ip = ipaddres.ToString();
+                Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                Debug.Log(ipaddres.ToString());
+                Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            }
+            
+            client = new TcpClient(ip, 8865);
             s = client.GetStream();
 
             //�������� � ���������
-			bool isOk = true;
-			while(isOk)
-			{
-				//Debug.Log("111");
-				isOk = sRead();
-				
-				int counter = 10;
-				while(!isNeedSend && counter != 0)
-				{
-					--counter;
-					Thread.Sleep(10);
-				}
-				
-				sWrite();
-				formatter.Serialize(s, new Mark(Mark.WAIT_SWAP));
-				
-				isNeedSend = false;
-			}
-		} catch (Exception e){
+            bool isOk = true;
+            while(isOk)
+            {
+                //Debug.Log("111");
+                isOk = sRead();
+                
+                int counter = 10;
+                while(!isNeedSend && counter != 0)
+                {
+                    --counter;
+                    Thread.Sleep(10);
+                }
+                
+                sWrite();
+                formatter.Serialize(s, new Mark(Mark.WAIT_SWAP));
+                
+                isNeedSend = false;
+            }
+        } catch (Exception e){
             Debug.Log(e);
         } finally {
-			if (client != null)
-				client.Close();
-		    SceneManager.LoadScene(0);
-		}
-	}
-	
-	bool sRead()
-	{
-		bool isOk = true;
-		bool swap = false;
-		while(!swap)
-		{
-			Mark mark = (Mark)formatter.Deserialize(s);
-			switch (mark.getType())
-			{
-				case Mark.WAIT_HZ:
-				case Mark.WAIT_EXIT:{
-					isOk = false;
-				}; break;
-				case Mark.WAIT_VECTOR3: {
-					//V3 t = (V3)formatter.Deserialize(s);
-				}; break;
-				case Mark.WAIT_SWAP: {
-					swap = true;
-				}; break;
-				case Mark.WAIT_START: {
-					inGame = true;
-				}; break;
-				case Mark.WAIT_RESTART: {
-					inGame = true; //��, ����� �� ����� �������� (�� update)
-				}; break;
-				default: {
-					Debug.Log("Incorrect mark type = " + mark.getType());
-				}; break;
-			}
-		}
-		return isOk;
-	}
-	
-	volatile bool needSendWin = false;
-	public void SendWin()
-	{
-		needSendWin = true;
-	}
-	
-	volatile bool needSendGameover = false;
-	public void SendGameover()
-	{
-		needSendGameover = true;
-	}
-	
-	void sWrite()
-	{
-		formatter.Serialize(s, new Mark(Mark.WAIT_VECTOR3));
-		formatter.Serialize(s, pos);
-		
-		if(needSendWin)
-		{
-			needSendWin = false;
-			formatter.Serialize(s, new Mark(Mark.WAIT_WIN));
-		}
-			
-		if(needSendGameover)
-		{
-			needSendGameover = false;
-			formatter.Serialize(s, new Mark(Mark.WAIT_GAMEOVER));
-		}
-	}
-	
-	volatile V3 pos = new V3(Vector3.zero);
-	
-    void Update(){
-		isNeedSend = true;
-		if (inGame) 
-		{
-			inGame = false;
-			level.RestartGame();
-		}
-		pos = new V3(transform.position);
+            if (client != null)
+                client.Close();
+            isNeedExit = true;
+        }
     }
-	
-	void Stop(){
-		if (client != null)
-			client.Close();
-	}
+    
+    bool sRead()
+    {
+        bool isOk = true;
+        bool swap = false;
+        while(!swap)
+        {
+            Mark mark = (Mark)formatter.Deserialize(s);
+            switch (mark.getType())
+            {
+                case Mark.WAIT_HZ:
+                case Mark.WAIT_EXIT:{
+                    isOk = false;
+                }; break;
+                case Mark.WAIT_VECTOR3: {
+                    //V3 t = (V3)formatter.Deserialize(s);
+                }; break;
+                case Mark.WAIT_SWAP: {
+                    swap = true;
+                }; break;
+                case Mark.WAIT_START: {
+                    inGame = true;
+                }; break;
+                case Mark.WAIT_RESTART: {
+                    inGame = true; //��, ����� �� ����� �������� (�� update)
+                }; break;
+                default: {
+                    Debug.Log("Incorrect mark type = " + mark.getType());
+                }; break;
+            }
+        }
+        return isOk;
+    }
+    
+    volatile bool needSendWin = false;
+    public void SendWin()
+    {
+        needSendWin = true;
+    }
+    
+    volatile bool needSendGameover = false;
+    public void SendGameover()
+    {
+        needSendGameover = true;
+    }
+    
+    void sWrite()
+    {
+        formatter.Serialize(s, new Mark(Mark.WAIT_VECTOR3));
+        formatter.Serialize(s, pos);
+        
+        if(needSendWin)
+        {
+            needSendWin = false;
+            formatter.Serialize(s, new Mark(Mark.WAIT_WIN));
+        }
+            
+        if(needSendGameover)
+        {
+            needSendGameover = false;
+            formatter.Serialize(s, new Mark(Mark.WAIT_GAMEOVER));
+        }
+    }
+    
+    volatile V3 pos = new V3(Vector3.zero);
+    
+    void Update(){
+        isNeedSend = true;
+        if (inGame) 
+        {
+            inGame = false;
+            level.RestartGame();
+        }
+        pos = new V3(transform.position);
+        if (isNeedExit)
+            SceneManager.LoadScene(0);
+    }
+    
+    void Stop(){
+        if (client != null)
+            client.Close();
+    }
 }
