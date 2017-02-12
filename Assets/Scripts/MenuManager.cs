db2ead8d319a8dda84932f70d96ee90a9ee69be2
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-02-13 00:44:20 +0330

    new menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ЬутгManager : MonoBehaviour {
+
+    private static bool _firstTime = true;
+
+    void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
